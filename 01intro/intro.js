4a01dcd16932cc7a8e4d6e1960153f4902371bec
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-19 15:49:14 +0100

    lear n

--- 01intro/intro.js
+++ 01intro/intro.js
@@ -1,3 +1,3 @@
 var user = { name: "Richard", age: 10 };
 console.log("Richard");
-console.log(user.name);
+console.log(user.email);
--- 01intro/intro.ts
+++ 01intro/intro.ts
@@ -1,4 +1,4 @@
 let user = { name: "Richard", age: 10 };
 
 console.log("Richard");
-console.log(user.name);
+console.log(user.email);
