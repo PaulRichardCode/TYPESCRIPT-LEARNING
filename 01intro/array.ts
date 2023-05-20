ea1e09dceb0757aeba8a368b32ee4f7054b980b3
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-20 19:58:22 +0100

    papi

---
+++ 01intro/array.ts
@@ -0,0 +1,23 @@
+//writing a string in typescript
+
+const darray: string[] = [];
+
+darray.push("bado");
+
+const names: Array<string> = [];
+const namess: string[] = [];
+
+type user = {
+  name: string;
+  age: number;
+};
+
+const profile: user[] = [];
+
+profile.push({ name: "richy", age: 12 });
+
+let kali: number[][] = [
+  [1, 2, 3],
+  [1, 2, 3],
+  [1, 2, 21],
+];
