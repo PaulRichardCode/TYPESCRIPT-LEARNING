62bf7a2bcff79451bd3a063864c5d71dbfa18411
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-20 11:55:00 +0100

    yes

--- 01intro/object.ts
+++ 01intro/object.ts
@@ -8,9 +8,14 @@ let john = {
 
 // sad({ name: 123, human: "daddy" });
 
-//***** Difference between let and type is type let you define and use the variable type in an objects
+//***** Difference between let and type is type let you define and use the variable type in an objects it also use semi-column ; instead of comma ,
 
 type Bingo = {
   name: string;
+  email: string;
   human: boolean;
 };
+
+let createUser = (user: Bingo) => {};
+
+createUser({ name: "", email: "", human: true });
