2163e87f122923afd26ba4682de86ae57854fd42
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-20 07:18:54 +0100

    okay

---
+++ 01intro/functions.js
@@ -0,0 +1,12 @@
+//let word: string = "west brook";
+function add(num) {
+    return num + 2;
+}
+add(5);
+function signUpUser(name, email, password) { }
+signUpUser("Richard", "Paul@gmail", 1234);
+//arrow functiom
+var signup = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+signup("h", "h@h.com");
---
+++ 01intro/functions.ts
@@ -0,0 +1,17 @@
+//let word: string = "west brook";
+
+function add(num: number): number {
+  return num + 2;
+}
+
+add(5);
+
+function signUpUser(name: string, email: string, password: number) {}
+
+signUpUser("Richard", "Paul@gmail", 1234);
+
+//arrow functiom
+
+const signup = (name: string, email: string, isPaid: boolean = false) => {};
+
+signup("h", "h@h.com");
