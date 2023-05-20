0366c5048345d48f976c3f92bb8c267e22da0287
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-20 11:46:59 +0100

    ss

---
+++ 01intro/object.js
@@ -0,0 +1,9 @@
+var john = {
+    name: "saheed",
+    class: 2,
+    human: true,
+};
+var sad = function (_a) {
+    var string = _a.name, boolean = _a.human;
+};
+sad({ name: 123, human: "daddy" });
---
+++ 01intro/object.ts
@@ -0,0 +1,16 @@
+let john = {
+  name: "saheed",
+  class: 2,
+  human: true,
+};
+
+// let sad = ({ name: string, human: boolean }) => {};
+
+// sad({ name: 123, human: "daddy" });
+
+//***** Difference between let and type is type let you define and use the variable type in an objects
+
+type Bingo = {
+  name: string;
+  human: boolean;
+};
