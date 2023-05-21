497d2e54173848fe2981920592d087c8d8cb6d9d
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-21 16:48:07 +0100

    x

--- 01intro/tuples.ts
+++ 01intro/tuples.ts
@@ -1,3 +1,7 @@
 // let bank: (string | number)[] = ["zenith", 10];
 
-let bank: [string, number] = ["rose", 20];
+// let bank: [string, number] = ["rose", 20];
+
+const banks: (string | boolean)[] = ["mommy", true];
+
+const rolls: [string, boolean] = ["logia", false];
