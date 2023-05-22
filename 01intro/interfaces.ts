c140cef0e74c3e00268e8d86ff63b93ad8777a2d
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-22 20:33:05 +0100

    okay

---
+++ 01intro/interfaces.ts
@@ -0,0 +1,31 @@
+//unlike forms
+type form = {
+  name: string;
+};
+
+//interface works slightly different
+
+interface forms {
+  name: string;
+  level: number;
+  readonly dil: number;
+  googleId?: number;
+  email: string;
+
+  //this are strictly made by interface not user
+  work(): string;
+  calc(num: number, name: string): number;
+}
+
+let studentInfo: forms = {
+  name: "",
+  level: 25,
+  dil: 23,
+  email: "h@mail.com",
+  work() {
+    return "adekune gold";
+  },
+  calc(number: 25, name: "richy") {
+    return 12;
+  },
+};
